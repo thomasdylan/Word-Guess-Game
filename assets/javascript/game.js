5cb0be14d84f672b74bda07969c4c0a05acea6cc
Dylan Thomas <thomasdylan76@gmail.com>
2019-08-24 23:39:24 -0600

    Added comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
+//List of words for game
 const wordJar = ["AWKWARD", "BAGPIPES", "BANJO", "BUNGLER", "CROQUET", "CRYPT", "DWARVES", "FERVID", "FISHHOOK",
 "FJORD", "GAZEBO", "GYPSY", "HAIKU", "HAPHAZARD", "HYPHEN", "IVORY", "JAZZY", "JIFFY", "JINX", "JUKEBOX", "KAYAK",
 "KIOSK", "KLUTZ", "MEMENTO", "MYSTIFY", "NUMBSKULL", "OSTRACIZE", "OXYGEN", "PAJAMA", "PHLEGM", "PIXEL", "POLKA",
 "QUAD", "QUIP", "RHYTHMIC", "ROGUE", "SPHINX", "SQUAWK", "SWIVEL", "TOADY", "TWELFTH", "UNZIP", "WAXY", "WILDEBEEST",
 "YACHT", "ZEALOUS", "ZIGZAG", "ZIPPY", "ZOMBIE"];
-
 //Grabs a random word from wordJar
 gameWord = wordJar[Math.floor(Math.random() * wordJar.length)];
 //Turns gameWord into underscores for each letter
