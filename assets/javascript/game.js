f98d6e739e3e0663dff119871d6768e6b863e3bc
Dylan Thomas <thomasdylan76@gmail.com>
2019-08-29 19:56:59 -0600

    Wrapped game into a function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,6 +17,15 @@ var answerWord = [];
 var remainingLetters;
 var gameWord;
 
+//Shows elements we want to display even when Game function is not running
+document.getElementById("losses").textContent = ("Losses: " + losses);
+document.getElementById("wins").textContent = ("Wins: " + wins);
+document.getElementById("underscore-word").textContent = ("Press any key to get started!");
+
+//Starts game function on key up
+
+
+
 function game() {
     //Grabs a random word from wordJar
     gameWord = wordJar[Math.floor(Math.random() * wordJar.length)];
@@ -29,9 +38,10 @@ function game() {
     //Sets the remaining letters to the length of the random word
     remainingLetters = answerWord.length;
 
-    
+    //Show elements we want during game
     document.getElementById("underscore-word").textContent = answerWord.slice("").join(" ");
     document.getElementById("remaining-guesses").textContent = ("Guesses Remaining: " + wrongGuesses);
+    
 
     //Get key user presses
     document.onkeyup = function (event) {
@@ -50,7 +60,12 @@ function game() {
         for (var i = 0; i < gameWord.length; i++) {
             if (userInput.toUpperCase() === gameWord[i]) {
                 answerWord[i] = userInput.toUpperCase();
-                remainingLetters--;
+                if(answerWord.includes(userInput.toUpperCase())) {
+                    console.log("only once");
+                } else {
+                    remainingLetters--;
+                }
+                
                 document.getElementById("underscore-word").textContent = answerWord.slice("").join(" ");
             }
         };
@@ -64,39 +79,49 @@ function game() {
             console.log("wrong guesses: " + wrongGuesses);
         }
         
-        //Checks to see if wrongGuesses has reached the max.  If so it changes the variable gameOver to true.
-        if (wrongGuesses <= 0) {
+        //Checks to see if remainingLetters has reached 0.  If so it changes the variable gameOver to true and adds one to wins.
+        if(remainingLetters = 0) {
             gameOver = true;
+            wins++;
+            document.getElementById("wins").textContent = ("Wins: " + wins);
         };
+
+        //Checks to see if wrongGuesses has reached 0.  If so it changes the variable gameOver to true and adds one to losses.
+        if (wrongGuesses = 0) {
+            gameOver = true;
+            losses++;
+            document.getElementById("losses").textContent = ("Losses: " + losses);
+        };
+
         console.log(gameOver);
         document.getElementById("guessed-letters").textContent = guessed.slice("").join(" ");
     }
 
-    //Unnecessary function.  Will be moved to an if statement that breaks the game
-    function resetGame() {
-        wrongGuesses = 8;
-        gameStart = false;
-    };
+    
 
-    //If restart button is clicked it refreshes the page.  Has to be outside game function.
-    function refreshPage() {
-        if (confirm("Are you sure? This will reset your score.")) {
-            window.location.reload();
-        }
-    };
+    
 
     console.log(gameWord);
     console.log(wrongGuesses);
 }
 
+//If restart button is clicked it refreshes the page.  Has to be outside game function.
+function refreshPage() {
+    if (confirm("Are you sure? This will reset your score.")) {
+        window.location.reload();
+    }
+};
+
 game();
+
+
 
 /*
 Choose a theme for your game! In the demo, we picked an 80s theme: 80s questions, 80s sound and an 80s aesthetic. You can choose any subject for your theme, though, so be creative!
-Use key events to listen for the letters that your players will type.
+X Use key events to listen for the letters that your players will type.
 Display the following on the page:
 Press any key to get started!
-Wins: (# of times user guessed the word correctly).
+X Wins: (# of times user guessed the word correctly).
 
 
 
@@ -105,9 +130,7 @@ X As the user guesses the correct letters, reveal them: m a d o _  _ a.
 
 
 
-Number of Guesses Remaining: (# of guesses remaining for the user).
+X Number of Guesses Remaining: (# of guesses remaining for the user).
 X Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
 After the user wins/loses the game should automatically choose another word and make the user play it.
 */
-
-//2 functions.  One to start game and one after game is ended.  Calls function multiple times
