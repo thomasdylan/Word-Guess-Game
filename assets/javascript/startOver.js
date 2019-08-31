d0dcd2fea278c28129b406390d30ed001a4bd85c
Dylan Thomas <thomasdylan76@gmail.com>
2019-08-31 13:05:46 -0600

    Once this script is finished it will be renamed to game and game deleted.

--- assets/javascript/startOver.js
+++ assets/javascript/startOver.js
@@ -24,3 +24,47 @@ gameWord = wordJar[Math.floor(Math.random() * wordJar.length)];
 document.getElementById("losses").textContent = ("Losses: " + losses);
 document.getElementById("wins").textContent = ("Wins: " + wins);
 document.getElementById("underscore-word").textContent = ("Press any key to get started!");
+
+function newWord() {
+    //Picks a random word from our list
+    gameWord = wordJar[Math.floor(Math.random() * wordJar.length)];
+
+    //Loops through the gameWord and creates an array of underscores
+    for (var i = 0; i < gameWord.length; i++) {
+        answerWord[i] = "_";
+    }
+
+    //
+    document.getElementById("underscore-word").textContent = answerWord.slice("").join(" ");
+    //Dev tools cheatcodes.
+    console.log(gameWord);
+}
+
+//When the window is loaded and a key is pressed this function calls newWord and set gameStart to true.
+//Wrapping this block inside an if function without the inner if doesn't work. Must be black magic.
+document.onkeyup = function () {
+    if (!gameStart) {
+        gameStart = true;
+        newWord();
+        console.log(gameStart);
+    }
+}
+
+/*
+Choose a theme for your game! In the demo, we picked an 80s theme: 80s questions, 80s sound and an 80s aesthetic. You can choose any subject for your theme, though, so be creative!
+Use key events to listen for the letters that your players will type.
+Display the following on the page:
+Press any key to get started!
+Wins: (# of times user guessed the word correctly).
+
+
+
+If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
+As the user guesses the correct letters, reveal them: m a d o _  _ a.
+
+
+
+Number of Guesses Remaining: (# of guesses remaining for the user).
+Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
+After the user wins/loses the game should automatically choose another word and make the user play it.
+*/
